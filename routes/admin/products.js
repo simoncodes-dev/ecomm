01bfd9eaa3fd81624b2054ee7c28e5bd836ddedd
Simon Fraser <simon@simoncodes.dev>
2022-06-20 13:47:27 -0700

    Add middleware, basic products rendering

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,29 +1,34 @@
 const express = require('express')
-const { validationResult } = require('express-validator')
+const multer = require('multer')
 
+const { handleErrors } = require('./middlewares')
 const productsRepo = require('../../repositories/products')
 const newProductTemplate = require('../../views/admin/products/new')
+const productsIndexTemplate = require('../../views/admin/products/index')
 const { requireProductTitle, requireProductPrice } = require('./validators')
 
 const router = express.Router()
+const upload = multer({ storage: multer.memoryStorage() })
 
-router.get('/admin/products', (req, res) => {
-
+router.get('/admin/products', async (req, res) => {
+    const products = await productsRepo.getAll()
+    res.send(productsIndexTemplate({ products }))
 }) 
 
 router.get('/admin/products/new', (req, res) => {
     res.send(newProductTemplate({}))
 })
 
-router.post('/admin/products/new', [requireProductTitle, requireProductPrice],
-    (req, res) => {
-        const errors = validationResult(req)
-        
-        req.on('data', data => {
-            console.log(data.toString())
-        })
+router.post('/admin/products/new', upload.single('image'), [requireProductTitle, requireProductPrice],
+    handleErrors(newProductTemplate), 
+    async (req, res) => {
 
-        res.send('submitted')
+        const image = req.file.buffer.toString('base64')
+        const { title, price } = req.body
+
+        await productsRepo.create({ title, price, image })
+
+        res.redirect('/admin/products')
     }
 )
 
